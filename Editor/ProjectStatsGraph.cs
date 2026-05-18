20a749e6bd99a4ba0c33ebc99fe36307bf5ae8bb
Aireus <theonewhohat3s@gmail.com>
2026-05-17 21:27:22 -0500

    fix jagged lines in line graph

--- Editor/ProjectStatsGraph.cs
+++ Editor/ProjectStatsGraph.cs
@@ -254,7 +254,7 @@ public static class ProjectStatsGraph
                     float y0        = GetY(graphRect, prevCount, yMax);
 
                     if (prevCount > 0 || currCount > 0)
-                        Handles.DrawAAPolyLine(2f, new Vector3(x0, y0), new Vector3(x1, y1));
+                        Handles.DrawAAPolyLine(2f, new Vector3(x0, y0), new Vector3(x, y));
                 }
 
                 if (currCount > 0)
@@ -272,7 +272,7 @@ public static class ProjectStatsGraph
         {
             float hx = GetX(graphRect, closestIndex, count);
             Handles.color = AxisColor;
-            Handles.DrawAAPolyLine(2f, new Vector3(x0, y0), new Vector3(x1, y1));
+            Handles.DrawAAPolyLine(1f, new Vector3(hx, graphRect.y), new Vector3(hx, graphRect.yMax));
         }
 
         Handles.EndGUI();
